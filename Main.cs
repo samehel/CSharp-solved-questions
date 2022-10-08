adb557175fa315716dbc75d3f01652fd2fafae21
Sameh <89010606+samehel@users.noreply.github.com>
2022-10-08 21:05:37 +0200

    Update Main.cs
    removed extra commented lines and spaces

--- Main.cs
+++ Main.cs
@@ -23,30 +23,30 @@ namespace Main
 
             Basic B = new Basic();
 
-            // Q1
-            //B.helloWorld();
-            // Q2
-            //Console.WriteLine(B.sum2());            
-            // Q3
-            //Console.WriteLine(B.divide2());
-            // Q4
-            //B.printResults();
-            // Q5
-            //B.swapNum();
-            // Q6
-            //Console.WriteLine(B.multiply3());
-            // Q7
-            //B.AllOps2();
-            // Q8
-            //B.multiplicationTable();
-            // Q9
-            //Console.WriteLine(B.FindAvg4());
-            // Q10
-            //B.performFormula();
-            // Q11
-            //B.olderThan();
-            // Q12
-            //B.seperate4Lines();
+//             Q1
+               B.helloWorld();
+//             Q2
+               Console.WriteLine(B.sum2());            
+//             Q3
+               Console.WriteLine(B.divide2());
+//             Q4
+               B.printResults();
+//             Q5
+               B.swapNum();
+//             Q6
+               Console.WriteLine(B.multiply3());
+//             Q7
+               B.AllOps2();
+//             Q8
+               B.multiplicationTable();
+//             Q9
+               Console.WriteLine(B.FindAvg4());
+//             Q10
+               B.performFormula();
+//             Q11
+               B.olderThan();
+//             Q12
+               B.seperate4Lines();
 
             /**
             *  BASIC ALGORITHM
@@ -55,65 +55,65 @@ namespace Main
             Basic_Algorithm BA = new Basic_Algorithm();
 
             // Q1
-            //Console.WriteLine(BA.trippleSum(1, 2));
-            //Console.WriteLine(BA.trippleSum(3, 2));
-            //Console.WriteLine(BA.trippleSum(2, 2));
+            Console.WriteLine(BA.trippleSum(1, 2));
+            Console.WriteLine(BA.trippleSum(3, 2));
+            Console.WriteLine(BA.trippleSum(2, 2));
 
             // Q2
-            //Console.WriteLine(BA.absoluteDifference(53));
-            //Console.WriteLine(BA.absoluteDifference(30));
-            //Console.WriteLine(BA.absoluteDifference(51));
+            Console.WriteLine(BA.absoluteDifference(53));
+            Console.WriteLine(BA.absoluteDifference(30));
+            Console.WriteLine(BA.absoluteDifference(51));
 
             // Q3
-            //Console.WriteLine(BA.thirtyChecker(30, 0));
-            //Console.WriteLine(BA.thirtyChecker(25, 5));
-            //Console.WriteLine(BA.thirtyChecker(20, 30));
-            //Console.WriteLine(BA.thirtyChecker(20, 25));
+            Console.WriteLine(BA.thirtyChecker(30, 0));
+            Console.WriteLine(BA.thirtyChecker(25, 5));
+            Console.WriteLine(BA.thirtyChecker(20, 30));
+            Console.WriteLine(BA.thirtyChecker(20, 25));
 
             // Q4
-            //Console.WriteLine(BA.withinRange(103));
-            //Console.WriteLine(BA.withinRange(90));
-            //Console.WriteLine(BA.withinRange(89));
+            Console.WriteLine(BA.withinRange(103));
+            Console.WriteLine(BA.withinRange(90));
+            Console.WriteLine(BA.withinRange(89));
 
             // Q5
-            //Console.WriteLine(BA.addToString("if else"));
-            //Console.WriteLine(BA.addToString("else"));
+            Console.WriteLine(BA.addToString("if else"));
+            Console.WriteLine(BA.addToString("else"));
 
             // Q6
-            //Console.WriteLine(BA.removeChar("Python", 1));
-            //Console.WriteLine(BA.removeChar("Python", 0));
-            //Console.WriteLine(BA.removeChar("Python", 4));
+            Console.WriteLine(BA.removeChar("Python", 1));
+            Console.WriteLine(BA.removeChar("Python", 0));
+            Console.WriteLine(BA.removeChar("Python", 4));
 
             // Q7
-            //Console.WriteLine(BA.swapChars("abcd"));
-            //Console.WriteLine(BA.swapChars("a"));
-            //Console.WriteLine(BA.swapChars("xy"));
+            Console.WriteLine(BA.swapChars("abcd"));
+            Console.WriteLine(BA.swapChars("a"));
+            Console.WriteLine(BA.swapChars("xy"));
 
             // Q8
-            //BA.repeatString("C Sharp");
-            //BA.repeatString("JS");
-            //BA.repeatString("a");
+            BA.repeatString("C Sharp");
+            BA.repeatString("JS");
+            BA.repeatString("a");
 
             // Q9
-            //Console.WriteLine(BA.moveToFrontAndBack("Red"));
-            //Console.WriteLine(BA.moveToFrontAndBack("Green"));
-            //Console.WriteLine(BA.moveToFrontAndBack("1"));
+            Console.WriteLine(BA.moveToFrontAndBack("Red"));
+            Console.WriteLine(BA.moveToFrontAndBack("Green"));
+            Console.WriteLine(BA.moveToFrontAndBack("1"));
 
             // Q10
-            //Console.WriteLine(BA.multiple37(3));
-            //Console.WriteLine(BA.multiple37(14));
-            //Console.WriteLine(BA.multiple37(12));
-            //Console.WriteLine(BA.multiple37(37));
+            Console.WriteLine(BA.multiple37(3));
+            Console.WriteLine(BA.multiple37(14));
+            Console.WriteLine(BA.multiple37(12));
+            Console.WriteLine(BA.multiple37(37));
 
             // Q11
-            //Console.WriteLine(BA.add3("Python"));
-            //Console.WriteLine(BA.add3("JS"));
-            //Console.WriteLine(BA.add3("Code"));
+            Console.WriteLine(BA.add3("Python"));
+            Console.WriteLine(BA.add3("JS"));
+            Console.WriteLine(BA.add3("Code"));
 
             // Q12
-            //Console.WriteLine(BA.startsWithCSharp("C# Sharp"));
-            //Console.WriteLine(BA.startsWithCSharp("C#"));
-            //Console.WriteLine(BA.startsWithCSharp("C++"));
+            Console.WriteLine(BA.startsWithCSharp("C# Sharp"));
+            Console.WriteLine(BA.startsWithCSharp("C#"));
+            Console.WriteLine(BA.startsWithCSharp("C++"));
 
             /**
             *  DATA TYPES
@@ -122,37 +122,37 @@ namespace Main
             Data_Types DT = new Data_Types();
 
             // Q1
-            //DT.inverse('b', 'a', 't');
+            DT.inverse('b', 'a', 't');
 
             // Q2
-            //DT.printTriangle(6, 6);
+            DT.printTriangle(6, 6);
 
             // Q3
-            //DT.login();
+            DT.login();
 
             // Q4
-            //DT.calculator();
+            DT.calculator();
 
             // Q5
-            //DT.areaOfCircle();
+            DT.areaOfCircle();
 
             // Q6
-            //DT.calculateEquation();
+            DT.calculateEquation();
 
             // Q7
-            //DT.calculateSpeed();
+            DT.calculateSpeed();
 
             // Q8
-            //DT.areaOfSphere();
+            DT.areaOfSphere();
 
             // Q9
-            //DT.checkInputSymbol();
+            DT.checkInputSymbol();
 
             // Q10
-            //DT.EvenOrOdd();
+            DT.EvenOrOdd();
 
             // Q11
-            //DT.convertToBinary();
+            DT.convertToBinary();
 
             /**
             *  CONDITIONAL STATEMENTS
@@ -160,40 +160,40 @@ namespace Main
             Conditional_Statements CS = new Conditional_Statements();
 
             // Q19
-            //CS.gradeDescription();
+            CS.gradeDescription();
 
             // Q22
-            //CS.monthNumToString();
+            CS.monthNumToString();
 
             // Q8
-            //CS.findLargest();
+            CS.findLargest();
 
             // Q3
-            //CS.numSign();
+            CS.numSign();
 
             // Q6
-            //CS.displayN();
+            CS.displayN();
 
             // Q2
-            //CS.oddOrEven();
+            CS.oddOrEven();
 
             // Q18
-            //CS.generateEletricBill();
+            CS.generateEletricBill();
 
             // Q24
-            //CS.calculateShapeArea();
+            CS.calculateShapeArea();
 
             // Q25
-            //CS.calculator();
+            CS.calculator();
 
             // Q20
-            //CS.numToDay();
+            CS.numToDay();
 
             // Q17
-            //CS.calculateProfitLoss();
+            CS.calculateProfitLoss();
 
             // Q12
-            //CS.generateDivision();
+            CS.generateDivision();
 
             /**
             *  For Loop
@@ -202,19 +202,19 @@ namespace Main
             ForLoop FL = new ForLoop();
 
             // Q3
-            //FL.displayNTerms();
+            FL.displayNTerms();
 
             // Q5
-            //FL.cubeNum();
+            FL.cubeNum();
 
             // Q6
-            //FL.timesTable();
+            FL.timesTable();
 
             // Q8
-            //FL.oddNums();
+            FL.oddNums();
 
             // Q15
-            //FL.factorial();
+            FL.factorial();
 
             /**
             *  Array
@@ -223,19 +223,19 @@ namespace Main
             _Array.Array A = new _Array.Array();
 
             // Q1
-            //A.storeElements();
+            A.storeElements();
 
             // Q2
-            //A.reverseArray();
+            A.reverseArray();
 
             // Q3
-            //A.sumElements();
+            A.sumElements();
 
             // Q4
-            //A.copyElements();
+            A.copyElements();
 
             // Q6
-            //A.uniqueElements();
+            A.uniqueElements();
 
             /**
              * String
@@ -244,13 +244,13 @@ namespace Main
             _String.String S = new _String.String();
 
             // Q7
-            //S.stringInfo();
+            S.stringInfo();
 
             // Q13
-            //S.cutString();
+            S.cutString();
 
             // Q14
-            //S.checkStringContents();
+            S.checkStringContents();
 
             /**
              * Functions
@@ -259,13 +259,7 @@ namespace Main
             Functions F = new Functions();
 
             // Q7
-            //F.power();
-
-            /**
-             * LINQ
-             **/
-
-
+            F.power();
 
         }
     }
